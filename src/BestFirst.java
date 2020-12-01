23eeb3156ec535ba19285a425a2103477de9732d
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-12-01 22:54:32 +0000

    p

--- src/BestFirst.java
+++ src/BestFirst.java
@@ -8,7 +8,7 @@ class BestFirst {
         private  Ilayout layout;
         private  State father;
         private List<State> childs=new ArrayList<>();
-        private double n=0,w=0;
+        private double n=0,w=0,l=0;
         private boolean final_node=false;
         private static final double c=Math.sqrt(2);
         private int max;
@@ -82,7 +82,7 @@ class BestFirst {
         i++;
         actual=new State(s,null);
         State root=actual;
-        int playouts=0,limit=10000;
+        int playouts=0,limit=50;//1000
         while(playouts<limit){
             if(!actual.childs.isEmpty()){
                 actual=selection(actual);
@@ -108,15 +108,19 @@ class BestFirst {
     }
 
     private State bestmove(State s) throws CloneNotSupportedException {
-       /* for(State suc:s.childs){
-            System.out.println("sim: "+suc.n+"\n"+suc);
+        for(State suc:s.childs){
+            System.out.println("sim: "+suc.n+" win:"+suc.w+" uct: "+suc.uct()+"\n"+suc);
             for(State suc_suc:suc.childs){
                 //System.out.println("sim: "+suc_suc.n+"\n"+suc_suc);
             }
-        }*/
+        }
         State res=Collections.max(s.childs, new Comparator<State>() {
                 @Override
                 public int compare(State z1, State z2) {
+                    if(z1.n==z2.n && z1.l<z2.l)
+                        return 1;
+                    if(z1.n==z2.n && z1.l>z2.l)
+                        return -1;
                     if (z1.n > z2.n)
                         return 1;
                     if (z1.n < z2.n)
@@ -145,16 +149,27 @@ class BestFirst {
         return actual;
     }
 
+    private int score(int x){
+        if(x>0){
+            return 1;
+        }else if(x==0){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+
     private State backpropagation(State actual2, int w, int ii) {
+        int score=score(w);
         while(actual2.father!=null){
-            if(w>0)
-                actual2.w+=w;
+            actual2.w+=score;
             actual2.n+=ii;
             actual2=actual2.father;
             w=-w;
+            score=score(w);
         }
-        if(w>0)
-            actual2.w+=w;
+        actual2.w+=w;
+        if(w<0) actual2.l++;
         actual2.n+=ii;
         return actual2;
     }
