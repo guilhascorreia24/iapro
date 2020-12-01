9ca9fa5f8f52882148a0b62b43c8985316777da2
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-12-01 20:58:52 +0000

    u

--- src/BestFirst.java
+++ src/BestFirst.java
@@ -1,44 +1,33 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 class BestFirst {
     static class State {
-        private  Ilayout layout,goal;
+        private  Ilayout layout;
         private  State father;
         private List<State> childs=new ArrayList<>();
-        private double n=0,w=0,g,f,h;
+        private double n=0,w=0;
+        private boolean final_node=false;
         private static final double c=Math.sqrt(2);
 
-        public State( Ilayout l,  State n,Ilayout goal) throws CloneNotSupportedException {
+        public State( Ilayout l,  State n) {
             layout = l;
             father = n;
-            this.goal=goal;
-            if(n==null){
-                g=0;
-            }else{
-                g=father.g+l.getG();
+            if(layout.verifywinner()!=-2){
+                final_node= true;
             }
-            h=layout.getH(goal);
-
         }
 
         public String toString() {
             return layout.toString();
         }
 
-        public boolean isGoal(){
-            return layout.isGoal(goal);
-        }
-
         private double uct() {
             if(n==0) return Double.MAX_VALUE;
             return (double)((w/n)+c*Math.sqrt(Math.log(father.n)/n));
         }
-
-
 
         public State BestUCT(){
             List<State> k=childs;
@@ -57,107 +46,126 @@ class BestFirst {
             return (Board)layout;
         }
 
-		public int getG() {
-			return (int) g;
-		}
-
     }
-    
     private State actual;
     public boolean end_game=false;
     private int i=0;
     public String winner="";
-    private int hard=200,med=20,ez=5,lvl;//190   ver melhor os niveis
-
+    private int hard=23,med=10,ez=1,lvl;
 
     final private List<State> sucessores( State n) throws CloneNotSupportedException { //listar os filhos que interessam
          List<State> sucs = new ArrayList<>();
          List<Ilayout> children = n.layout.children();
         for (Ilayout e : children) {
             if (n.father == null || !e.equals(n.father.layout)) {
-                State nn = new State(e, n, obj);
+                State nn = new State(e, n);
                 sucs.add(nn);
             }
         }
         return sucs;
     }
-
-
-    final public Iterator<State> mcts(Ilayout s,Ilayout s2) throws CloneNotSupportedException { // algoritmo bfs
-        actual=new State(s,null,s2);
-        while(!actual.isGoal()){
+    public void lvl(int x){
+        if(x==1) lvl=ez;
+        else if(x==2) lvl=med;
+        else if(x==3) lvl=hard;
+    }
+    final public Board BestNextMove(Ilayout s) throws CloneNotSupportedException { // algoritmo bfs
+        i++;
+        actual=new State(s,null);
+        State root=actual;
+        int playouts=0,limit=lvl;
+        while(playouts<limit){
             if(!actual.childs.isEmpty()){
                 actual=selection(actual);
             }
-            System.out.println(actual);
-            if(actual.isGoal()){
-                break;
-            }
-            actual.childs=sucessores(actual);
+            if(!actual.final_node)
+                actual.childs=sucessores(actual);
             actual=simulation(actual);
+            playouts++;
         }
-
-        List<State> sol=new ArrayList<State>();
-        while(actual!=null){
-            sol.add(actual);
-            actual=actual.father;
+        if(!root.final_node)
+            actual=bestmove(root);
+        else{
+            end_game=true;
         }
-        Collections.reverse(sol);
-        return sol.iterator();
-        
-        //return (Board)actual.layout;
+        return (Board)actual.layout;
     }
 
-<<<<<<< HEAD
     private State bestmove(State s) throws CloneNotSupportedException {
-=======
-
-    private State back(State s) throws CloneNotSupportedException {
-        /*for(State suc:s.childs){
-            System.out.println(suc+"\n"+suc.n);
-        }*/
->>>>>>> 681dd40774bdb98acfd911f942995560af5decbb
-        State res=Collections.max(s.childs, new Comparator<State>() {
-            @Override
-            public int compare(State z1, State z2) {
-                if ((z1.n) > (z2.n)){
-                        return 1;
-                }
-                if (z1.n < z2.n)
-                    return -1;
-                return 0;
+        State res=s;
+        int max=0;
+        for(State suc:s.childs){
+            if(suc.final_node) {
+                res=suc;
+                break;
             }
-        });
+            List<State> sucs=sucessores(suc);
+            boolean next=false;
+            for(State suc_suc:sucs){
+                if(suc_suc.final_node && suc_suc.layout.verifywinner()==1){
+                    next=true;
+                }
+                //System.out.println(suc_suc+"\n"+next);
+            }
+            //System.out.println(suc+"\n"+suc.n);
+            if(!next && suc.n>max){
+                res=suc;
+                max= (int) res.n;
+            }
+        }
+        if(res.equals(s)){
+            res=Collections.max(s.childs, new Comparator<State>() {
+                @Override
+                public int compare(State z1, State z2) {
+                    if (z1.n > z2.n)
+                        return 1;
+                    if (z1.n < z2.n)
+                        return -1;
+                    return 0;
+                }
+            });
+        }
+
+        if(res.final_node){
+            end_game=true;
+            if(res.layout.verifywinner()!=0){
+                if(i%2==0) winner="PC2";
+                else winner="PC1";
+            }else{
+                winner="draw";
+            }
+        }
         return res;
     }
 
     private State simulation(State s) throws CloneNotSupportedException {
         actual=s;
-        int w=0;
+        int w=s.layout.verifywinner();
         for(State suc:s.childs){
             s=suc;
-            while(s.g<=actual.h || !s.isGoal()){
+            while(!s.final_node){
                 List<State> sucs=sucessores(s);
                 int rn=(int)(Math.random()%sucs.size());
                 s=sucs.get(rn);
             }
-            if(s.isGoal()) w=1;
+            w=s.layout.verifywinner();
             actual=backpropagation(s,w,1);
         }
-        if(actual.isGoal()){
-            w=1;
-        }
+        if(actual.final_node)
             actual=backpropagation(s,w,1);
         return actual;
     }
 
     private State backpropagation(State actual2, int w, int i) {
         while(actual2.father!=null){
-            actual2.w+=w;
+            if(w>0)
+                actual2.w+=w;
             actual2.n+=i;
             actual2=actual2.father;
+            w=-w;
         }
-        actual2.w+=w;
+        if(w>0)
+            actual2.w+=w;
         actual2.n+=i;
         return actual2;
     }
